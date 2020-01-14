97ecb5e691892aa95abfa7fc85e0629fc4c27bb0
Shashank Mungantiwar <shashank.mungantiwar@perficient.com>
2020-01-14 17:37:29 +0530

    Stockdata-Backend-2 : Added Swagger

---
+++ src/main/java/com/app/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.app.config;
+
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	private final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		logger.debug("Configuring Swagger for Application...!!!");
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.app"))
+				.build()
+				.apiInfo(apiInfo());
+		
+	}
+	
+	private ApiInfo apiInfo() {
+		logger.debug("Creating APIInfo for Swagger...!!!");
+		return new ApiInfo(
+				"StockData",
+				"Stock Data Application To Maintain Records", 
+				"1.0", 
+				"Free to Use", 
+				new Contact("Shashank Mungantiwar","https://creative2brand.com","srmungantiwar1@gmail.com"), 
+				"API License",
+				"https://creative2brand.com",
+				Collections.emptyList());
+	}
+}
--- src/main/java/com/app/resource/Greeting.java
+++ src/main/java/com/app/resource/Greeting.java
@@ -1,15 +1,20 @@
-package com.app;
+package com.app.resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@RequestMapping("/api")
 public class Greeting {
 	private final Logger logger = LoggerFactory.getLogger(Greeting.class);
 	
 	@GetMapping(value = "/hello")
+	@ApiOperation(value = "Gets all user groups",notes = "Greeting for testing", response = String.class)
 	public String greeting() {
 		logger.info("Hello:::::::::::::::::::::");
 		return "Hello";
