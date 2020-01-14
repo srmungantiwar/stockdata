094e5235270f41cd406319e9799e3dd8ceb05bf3
Shashank Mungantiwar <shashank.mungantiwar@perficient.com>
2020-01-14 13:22:51 +0530

    Stockdata-Backend-1 : Added logger

--- src/main/java/com/app/Greeting.java
+++ src/main/java/com/app/Greeting.java
@@ -1,13 +1,17 @@
 package com.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Greeting {
+	private final Logger logger = LoggerFactory.getLogger(Greeting.class);
 	
 	@GetMapping(value = "/hello")
 	public String greeting() {
+		logger.info("Hello:::::::::::::::::::::");
 		return "Hello";
 	}
 }
